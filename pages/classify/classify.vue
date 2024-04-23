41d78896c5bbb5ebb0bccde3028b716c960f88ae
sunling <sling2007@163.com>
2024-04-23 22:46:17 +0800

    init

---
+++ pages/classify/classify.vue
@@ -0,0 +1,26 @@
+<template>
+	
+	<view class="classifyPage">
+		
+		<view class="classify">
+			<theme-item  v-for="item in 16"></theme-item>
+		</view>
+	</view>
+</template>
+
+<script setup>
+	
+</script>
+
+<style lang="scss" scoped>
+
+	.classify{
+		width: 750rpx;
+		padding: 50rpx 0;
+		margin:30rpx;
+		display: grid;
+		gap:15rpx;
+		grid-template-columns: repeat(3,1fr);
+	
+	}
+</style>
---
+++ pages/user/user.vue
@@ -0,0 +1,14 @@
+<template>
+	<view>
+		我的的的
+	</view>
+</template>
+
+<script setup>
+	
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
